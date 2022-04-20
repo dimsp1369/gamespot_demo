9b68f02b59ab0946a08990c337075e24ab204d46
Spiridonov <dmsp1369@gmail.com>
2022-04-19 22:09:14 -0700

    add some stiles for game

--- src/pages/game.js
+++ src/pages/game.js
@@ -5,8 +5,22 @@ const Game = () => {
     const game = useSelector(state => state.currentGameReducer.game)
 
     return (
-        <div>
-            {game.name}
+        <div className='Game-Container'>
+            <div className='GameDescription-Wrapper'>
+                <div className='GameDescription-Column_1'>
+                    <h1>{game.name}</h1>
+                    <p></p>
+                </div>
+                <div className='GameDescription-Column_2'>
+                    <div className='GameScreenShots'>
+                        {game.short_screenshots.slice(0, 4).map(img => <img src={img.image} key={img.id}
+                                                                            className='ScreenShot-Tile'/>)}
+                        <div className='ScreenShot-Tile_Blank'>
+                            <span> view all</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
